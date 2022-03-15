72e6e4f50b7594127275d557b4c40cec3aebde6f
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-15 10:25:52 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+julien@ubuntu:~/0x02$ cat 1-main.c
+#include "main.h"
+   clrscr();
+       int main(void){
+        for (ch= ‘A’; ch<= ‘Z’; ch++)
 
+             putchar(ch);
+
+             putchar (‘\n’ ) ;
+
+             /* print lowercase letters */
+
+             for (ch= ‘a’; ch<= ‘z’; ch++)
+
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+                  getch();
+       }
