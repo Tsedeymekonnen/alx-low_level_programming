a7c5ceb710a95064d248df49b663997dc06933ab
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-15 10:40:04 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,11 @@ julien@ubuntu:~/0x02$ cat 1-main.c
 #include "main.h"
    clrscr();
        int main(void){
+          /* Write a function that prints the alphabet, in lowercase, followed by a new line.
+
+           Prototype: void print_alphabet(void);
+You can only use _putchar twice in your code
+*/
         for (ch= ‘A’; ch<= ‘Z’; ch++)
 
              putchar(ch);
