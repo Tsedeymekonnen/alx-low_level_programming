679e5aa61b46d2919f3f76d030657da268864c53
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 11:53:41 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void print_diagonal(int n){
+   for (int i=0 ; i<=n ; i++) {
+      for(int j=i ;i<=n ; i++ ) {
+      printf("#")
+      }
+   }
+              printf("\n")
+}
+ int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
