beb5c7a5ad1b767fede47e15392f3701c35ec4b7
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 11:11:15 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+/* Write a function that prints the numbers, from 0 to 9, followed by a new line.y*/
+#include <stdio.h>
+ 
+void print_most_numbers(long n)
+{
+ 
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+ 
+ 
+    if else  (n==2 || n==4) {
+      return 0 ;
+      // continue
+ }
+ else  
+ return n ;
+}
+ 
+// Driver program to test above function
+int main()
+{
+  print_most_numbers( 5);
+    return (0);
+ 
+}
