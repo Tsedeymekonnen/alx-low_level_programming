92e83e3c7be402fdacb6f48c6f78317ff2fb5bd1
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 10:14:12 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main () {
+   int c = 'M';
+  
+    
+   if( isupper(c) ) {
+      printf("var1 = |%c| is uppercase character\n", var1 );
+     return(1);
+                 }
+  else {
+       return(0);
+   }
+
+}
