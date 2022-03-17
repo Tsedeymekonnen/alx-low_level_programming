de3b01c0de9e5ea22f3f70de8ac72002a5feba9c
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 11:19:58 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void more_numbers(){
+   int i;
+    for(i=1;i<=10;printf(" 01234567891011121314 \n",i++));
+    return 0;
+}
+ int main() 
+{
+ more_numbers();
+   return 0 ;
+}
