676d9b72edb355e452b134e745b4dee261c23c89
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 10:28:03 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main () {
+  /* 
+  Write a function that checks for a digit (0 through 9).
+  */ 
+   int c = '2';
+    
+   if( isdigit(c) ) {
+      printf("c= |%c| is a digit\n", var1 );
+     return(1)
+   } else {
+      printf("var1 = |%c| is not a digit\n", var1 );
+        return(0); 
+   }
+   return(0);
+}
