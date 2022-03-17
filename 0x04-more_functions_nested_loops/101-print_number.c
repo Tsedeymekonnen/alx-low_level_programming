2cf04cdfbd3b5114bd489bbcda06e67522c945db
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-17 12:22:53 +0300

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+void print_number(int n)
+{   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    }
+    int main() {
+    int n ;
+    void print_number(n);
+    return 0;
+}
