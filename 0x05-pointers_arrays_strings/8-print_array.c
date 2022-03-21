04b20cd8215c75d1e276a90d53bd206453a6e56f
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-21 08:40:34 +0300

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+/* Write a function that prints n elements of an array of integers, followed by a new line. */
+#include <stdio.h>
+#include "main"
+void print_array(int *a, int n)
+{
+	short c = 0;
+
+	while (n-- > 0)
+	{
+		printf("%d", a[c++]);
+		if (n != 0)
+			printf(", ");
+	}
+
+		printf("\n");
+}
+int main(void)
+{
+    int array[5];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
+}
