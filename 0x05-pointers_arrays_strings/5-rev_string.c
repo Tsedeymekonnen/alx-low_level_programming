00ed7feccf42fbc8768b5b7f0adab426a1fa140c
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-21 08:32:32 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+/* Write a function that reverses a string. */
+#include <stdio.h>
+#include "main"
+
+
+void rev_string(char *s)
+{
+	char *t = s;
+	char n[1000];
+	short c = 0;
+
+	while (*s != '\0')
+	{
+		n[c] = *s;
+		s++;
+		c++;
+	}
+	c = 0;
+
+	while (s > t)
+	{
+		s--;
+		*s = n[c];
+		c++;
+	}
+}
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
