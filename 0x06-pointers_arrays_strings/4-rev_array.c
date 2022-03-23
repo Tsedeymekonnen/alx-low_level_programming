16f002521ca88609bb3b8f2bd422a010c74269fc
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-23 14:00:30 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int tmp;
+
+	i = 0;
+	j = n - 1;
+
+	while (i < j)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
+	}
+}
