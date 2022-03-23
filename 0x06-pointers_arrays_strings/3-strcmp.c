7549a51a30a527788eea64ddd23ce9e3311999d6
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-23 13:56:35 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+/* Write a function that compares two strings. */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int R;
+
+	i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	R = s1[i] - s2[i];
+	return (R);
+}
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
