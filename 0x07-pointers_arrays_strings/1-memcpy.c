bdeb4d20985b91db83099de0d144e4f58bac650c
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-28 14:30:01 +0300

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*Write a function that locates a character in a string.
+ */
+
+char *_memcpy(char *dest, char *src)
+{
+
+
+	for (int i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
