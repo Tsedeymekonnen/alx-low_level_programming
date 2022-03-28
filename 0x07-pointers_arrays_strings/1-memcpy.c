9be4ce97ce01a8f370a43a64a1303bb42d0ee1f1
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-28 14:34:31 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,11 +4,11 @@
 *Write a function that locates a character in a string.
  */
 
-char *_memcpy(char *dest, char *src)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	
 
-
-	for (int i = 0; i < n; i++)
+	for (unsigned int i; = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
