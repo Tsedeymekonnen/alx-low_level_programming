7869e61240ef821d35a6541940a1feb991df39ec
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-28 14:48:58 +0300

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *Write a function that searches a string for any of a set of bytes.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
