8bba368b017491aded4e49512bd3e20289a83f47
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-28 14:23:08 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * Write a function that fills memory with a constant byte.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
