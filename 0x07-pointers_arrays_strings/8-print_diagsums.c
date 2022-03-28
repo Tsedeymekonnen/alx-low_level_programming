bdec56b2dbab3ed868bfb0c415d5543a7386210f
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-28 14:59:55 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * Write a function that prints the sum of the two diagonals of a square matrix of integers.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
