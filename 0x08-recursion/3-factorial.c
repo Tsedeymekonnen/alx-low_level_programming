581cc1956b756949b03f78cc80cd8d701f6cc605
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-30 12:59:50 +0300

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+#include <string.h>  
+/* Write a function that returns the factorial of a given number.*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
