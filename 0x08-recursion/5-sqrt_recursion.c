ee93ac57cc51ab79728b08463ce4e84ef03254a5
Tsedeymekonnen <88814281+Tsedeymekonnen@users.noreply.github.com>
2022-03-30 13:17:26 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+
+/* Write a function that returns the natural square root of a number.*/
+void  int _sqrt_recursion(int n){
+if (n < 0)
+		return (-1);
+}
+int _sqrt(int n, int i)
+{
+	if (n < 0){
+		return (-1);
+  }
+	else if ((i * i) > n){
+		return (-1);
+  }
+  else if ((i * i) < n){
+		return (-1);
+  }
+   
+  else {
+    i = sqrt(n);
+     	return (i);
+   }
+
